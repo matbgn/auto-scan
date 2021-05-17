05a232a7e527a55b6abdbcb51ba5f4826588b33e
Matthieu Borgognon <matrash@bqn.ch>
2021-05-17 12:04:51 +0200

    Add sudo privileges on launch to ensure network host access

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from send_email import *
 ts_now = '{:%Y-%m-%d_%H%M%S}'.format(datetime.datetime.now())
 file_with_ts = 'attachment_' + ts_now + '.pdf'
 
-subprocess.run(["scan-pdf/src/scan-pdf", "--flatbed", "--color-mode", "color", file_with_ts])
+subprocess.run(["scan-pdf/src/scan-pdf", "--color-mode", "color", file_with_ts])
 
 subprocess.run(["ocrmypdf", "-r", "--rotate-pages-threshold", "6", "-O", "3", file_with_ts, file_with_ts])
 
